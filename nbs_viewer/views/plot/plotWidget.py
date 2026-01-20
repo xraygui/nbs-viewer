b59ad78a24a8db2f87e20f0a10166326b02a51cf
Charles Titus <ctitus@bnl.gov>
2026-01-20 12:50:00 -0500

    Removing tight_layout calls from figure manager

--- nbs_viewer/views/plot/plotWidget.py
+++ nbs_viewer/views/plot/plotWidget.py
@@ -251,7 +251,7 @@ class MplCanvas(FigureCanvasQTAgg):
         height = event.size().height() / self.fig.dpi
         self.fig.set_size_inches(width, height)
         # Ensure layout is updated
-        self.fig.tight_layout()
+        # self.fig.tight_layout()
         self.draw()
 
     def plot_data(self, plotData):
@@ -303,7 +303,7 @@ class MplCanvas(FigureCanvasQTAgg):
             if len(x) > 0 and hasattr(plotData, "dimension_names"):
                 self.axes.set_xlabel(plotData.dimension_names[0])
 
-            self.fig.tight_layout()
+            # self.fig.tight_layout()
             if self._autoscale:
                 self.autoscale()
             self.updateLegend()
